c91c67b3265fe596d11833ed9be3f10b199d2647
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-05-12 12:29:50 -0400

    temp

--- scripts/geoLocation/geoLocation1.js
+++ scripts/geoLocation/geoLocation1.js
@@ -1,32 +1,43 @@
-navigator.geolocation ?
-  navigator.geolocation.getCurrentPosition(
-    position => {
-      const { latitude, longitude, altitude } = position.coords;
-      l(`https://www.google.com/maps/@${latitude},${longitude}`);
-      const map = L.map('wolfMap').setView([latitude, longitude], 1300);
-
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
-
-      L.marker([51.5, -0.09])
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.');
-    },
-    err => {
-      l(err);
-    }
-  ) : l('no geolocation');
 
 
 
-// d(globalThis)
+navigator.geolocation
+  ? navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
 
-//   e('errorMessage313')
+        l(`https://www.google.com/maps/@${latitude},${longitude}`);
+        const map = L.map('wolfMap').setView([latitude, longitude], 130);
 
-// t('banana')
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(map);
 
+        map.on('click', function (mapEvent) {
+          form.classList.remove('hidden');
+          inputDistance.focus();
+         
+        });
+      },
+      err => {
+        l(err);
+      }
+    )
+  : l('no geolocation');
+
+form.addEventListener('submit', function (e) {
+ let {lat, lng} = mapEvent.latlng
+          L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+          })).setPopupContent('Workout').openPopup()
+});
 
 let constructor = 'constructor';
 
