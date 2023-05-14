9ffb3fd4922ea524ca076ba4986f1a417398d8a1
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-05-14 14:30:50 -0400

    temp

--- scripts/classApp/classApp.js
+++ scripts/classApp/classApp.js
@@ -1,66 +1,58 @@
 import { getPosition, loadMap } from '../classWorkout/getPosition.js';
 class App {
-    _map
-    #mapEvent
+  _map;
+  _mapEvent;
   constructor(props) {
-    (this.workouts = props.workouts)
+    this.workouts = props.workouts;
     this._getPosition();
-    
+    form.addEventListener('submit', this._submitForm.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField);
   }
-  
+
   _getPosition = getPosition;
 
   _showFrom() {
-    this._map.on(
-      'click',
-     (mapEvent)=> {
-        form.classList.remove('hidden');
-        inputDistance.focus();
-        this.#mapEvent = mapEvent;
-      }
-    );
-    this._submitForm();
-  }
-  _submitForm() {
-    form.addEventListener('submit', e => {
-      e.preventDefault();
-      inputDistance.value =
-        inputDuration.value =
-        inputCadence.value =
-        inputElevation.value =
-          '';
-
-      let { latlng: {lat, lng} } = this.#mapEvent;
-      L.marker([lat, lng])
-        .addTo(this._map)
-        .bindPopup(
-          L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-          })
-        )
-        .setPopupContent('Workout')
-        .openPopup();
+    this._map.on('click', mapEvent => {
+      form.classList.remove('hidden');
+      inputDistance.focus();
+      this._mapEvent = mapEvent;
+    
     });
-    this._toggleElevationField();
+  }
+  _submitForm(e) {
+    e.preventDefault();
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+
+    let {
+      latlng: { lat, lng },
+    } = this._mapEvent;
+    L.marker([lat, lng])
+      .addTo(this._map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup',
+        })
+      )
+      .setPopupContent('Workout')
+      .openPopup();
   }
 
   _toggleElevationField() {
-    inputType.addEventListener('change', function () {
-      inputElevation
-        .closest('.form__row')
-        .classList.toggle('form__row--hidden');
-      inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-    });
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
   }
 
   _newWorkout() {}
 }
 let app = new App({ workouts: [] });
-
 
 let constructor = 'constructor';
 
