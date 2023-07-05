421f0ab969cadcafd409c19acfb2e813331983bf
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-07-04 21:24:19 -0400

    marker

--- scripts/classApp/marker.js
+++ scripts/classApp/marker.js
@@ -1,18 +1,20 @@
 function marker(tempObj){
-     L.marker([lat, lng])
-      .addTo(this._map)
-      .bindPopup(
-        L.popup({
-          maxWidth: 250,
-          minWidth: 50,
-          autoClose: false,
-          closeOnClick: false,
-          className: inputType.value === 'running' ? 'running-popup' : 'cycling-popup'
-          
-        })
-      )
-      .setPopupContent(` ${inputType.value}  ${tempObj.dateS}`)
-      .openPopup();
+
+    L.marker([tempObj.coords.lat, tempObj.coords.lng])
+    .addTo(this._map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 50,
+        autoClose: false,
+        closeOnClick: false,
+        className: tempObj.inputType === 'running' ? 'running-popup' : 'cycling-popup'
+        
+      })
+    )
+    .setPopupContent(` ${inputType.value}  ${tempObj.dateS}`)
+    .openPopup();
+
 }
 
 
--- scripts/classApp/newWorkout.js
+++ scripts/classApp/newWorkout.js
@@ -52,21 +52,22 @@ this._addWorkout(tempObj)
       inputElevation.value =
        313;
 
- 
-    L.marker([tempObj.coords.lat, tempObj.coords.lng])
-      .addTo(this._map)
-      .bindPopup(
-        L.popup({
-          maxWidth: 250,
-          minWidth: 50,
-          autoClose: false,
-          closeOnClick: false,
-          className: tempObj.inputType === 'running' ? 'running-popup' : 'cycling-popup'
+    marker.call(this, tempObj)
+
+    // L.marker([tempObj.coords.lat, tempObj.coords.lng])
+    //   .addTo(this._map)
+    //   .bindPopup(
+    //     L.popup({
+    //       maxWidth: 250,
+    //       minWidth: 50,
+    //       autoClose: false,
+    //       closeOnClick: false,
+    //       className: tempObj.inputType === 'running' ? 'running-popup' : 'cycling-popup'
           
-        })
-      )
-      .setPopupContent(` ${inputType.value}  ${tempObj.dateS}`)
-      .openPopup();
+    //     })
+    //   )
+    //   .setPopupContent(` ${inputType.value}  ${tempObj.dateS}`)
+    //   .openPopup();
 
       form.classList.add('hidden');
   };
