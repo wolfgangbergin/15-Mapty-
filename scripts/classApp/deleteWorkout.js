110c953eb623eb7b73011aa566824de043e6cb57
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-07-09 03:58:25 -0400

    deleteWorkingHalfWay

--- scripts/classApp/deleteWorkout.js
+++ scripts/classApp/deleteWorkout.js
@@ -3,8 +3,15 @@ function deleteWorkout(e){
 if (!(e.target.className === 'btn btn--small btn--delete')) return
 
 
-l(e.target.dataset.id)
-l(this)
+l(+e.target.dataset.id)
+l(this.workouts)
+
+let temp = this.workouts
+
+ let kim = this.workouts.filter((obj)=>obj.id !== +e.target.dataset.id)
+
+this.workouts = kim
+
 }
 
 export {deleteWorkout}
