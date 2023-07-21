f63200e81ac88551de161f8f714e8c4921e1c85e
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-07-21 02:16:36 -0400

    working

--- scripts/classApp/recenter.js
+++ scripts/classApp/recenter.js
@@ -1,11 +1,19 @@
-let tempCords = [
-  [42.41092308436314, -83.30192734669055],
-  [46.02851982368131, -87.40619137417148],
-  [ 33.58818317455442,  -117.86116809588643]
-];
+// let tempCords = [
+//   [42.41092308436314, -83.30192734669055],
+//   [46.02851982368131, -87.40619137417148],
+//   [ 33.58818317455442,  -117.86116809588643]
+// ];
 
 let recenterFunc = function () {
-    l(this.workouts)
+    let tempCords = [];
+    this.workouts.forEach(obj => {
+       let temp = [];
+       temp.push(obj.coords.lat);
+       temp.push(obj.coords.lng);
+         tempCords.push(temp);
+    });
+
+ if (tempCords.length === 0) return;
   this._map.fitBounds(tempCords);
 };
 
